1e69f88190bd71350fa6c42ae4a8e7d153668881
JeffreyLinnn <166924251+JeffreyLinnn@users.noreply.github.com>
2024-06-11 16:37:43 +0800

    BGM

--- Game.h
+++ Game.h
@@ -6,6 +6,7 @@
 #include <QTimer>
 #include <QObject>
 #include <QGraphicsPixmapItem>
+#include "BGM.h"  
 
 class RuneBoard;
 class Object;
@@ -16,7 +17,7 @@ class PlayerStatusBar;
 class CharacterSlot;
 class Battle;
 
-class Game: public QObject{
+class Game : public QObject {
     Q_OBJECT
 public:
     Game() = default;
@@ -29,13 +30,12 @@ public:
     QList<Object*> Object_list;
     QList<MouseListener*> MouseListeners;
     QList<TickListener*> TickListeners;
-    RuneBoard *getBoard() const;
-    Battle *getCurrentBattle() const;
+    RuneBoard* getBoard() const;
+    Battle* getCurrentBattle() const;
     int& ref_playerHp();
     void nextBattle();
-    CharacterSlot *getCharacterSlot() const;
+    CharacterSlot* getCharacterSlot() const;
     void setBackgroundImage(const QString &imagePath);
-
     void setPlayerHp(int newPlayerHp);
 
 private:
@@ -55,6 +55,7 @@ private:
     int playerHp = 0;
     int BattleIndex = 0;
     QGraphicsPixmapItem* backgroundItem;
+    BGM* bgm;  
 
 private slots:
     void update();
